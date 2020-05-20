f7d192f96b3a0910de55a20643d1f986281ee446
JossWritesCode <joscelynrose@gmail.com>
2020-05-19 22:10:00 -0400

    added create method

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -47,15 +47,31 @@ class HelloApiView(APIView):
 
 
 class HelloViewSet(viewsets.ViewSet):
-    """Test API ViewSet"""
+    """Test API ViewSet."""
+
+    serializer_class = serializers.HelloSerializer
 
     def list(self, request):
-        """Return a hello message"""
+        """Return a hello message."""
 
         a_viewset = [
-            'Uses actions (list, create, retrieve, update, partial_update)',
-            'Automatically maps to URLs using Routers',
-            'Provides more functionality with less code',
+            'Uses actions (list, create, retrieve) instead of HTTP methods',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code'
         ]
 
-        return Response({'message': 'Hello!', 'a_viewset': a_viewset})                                                                                                   a_viewset})
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
+
+    def create(self, request):
+        """Create a new hello message"""
+        serializer = self.serializer_class(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}'
+            return Response({'message': message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )

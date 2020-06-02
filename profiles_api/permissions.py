031548d7c19ab5a9065e4700fd8b587bc744f0a8
JossWritesCode <joscelynrose@gmail.com>
2020-06-02 15:27:01 -0400

    updated user permissions for updating own status

--- profiles_api/permissions.py
+++ profiles_api/permissions.py
@@ -11,3 +11,13 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
         # otherwise, return the result of whether user is trying to update own profile
         return obj.id == request.user.id
+
+
+class UpdateOwnStatus(permissions.BasePermission):
+    """Allow users to update their own status"""
+
+    def has_object_permission(self, request, view, obj):
+        """Check the user is trying to update their own status"""
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.user_profile.id == request.user.id

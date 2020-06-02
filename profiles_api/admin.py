48e9f0654995dacb9f3b4b4713fb92c2cf19968e
JossWritesCode <joscelynrose@gmail.com>
2020-06-02 15:11:14 -0400

    set the feed items to an endpoints

--- profiles_api/admin.py
+++ profiles_api/admin.py
@@ -1,7 +1,8 @@
+# Register your models here.
 from django.contrib import admin
 
 from profiles_api import models
 
 
 admin.site.register(models.UserProfile)
-# Register your models here.
+admin.site.register(models.ProfileFeedItem)
